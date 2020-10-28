837b45155c764e4a8e6f1d1b6659a4457344eef8
Koray Tanulku <ktanulku@omers.com>
2020-10-27 20:18:21 -0400

    contentful post render

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,26 @@
 import Head from 'next/head'
+
+import { fetchEntries } from '@utils/contentfulPosts'
+
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Post from '@components/Post'
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Next + Contentful Starter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header />
+        <div className="posts">
+          {posts.map((p) => {
+            return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
+          })}
+        </div>
       </main>
 
       <Footer />
@@ -27,7 +33,6 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-
         main {
           padding: 5rem 0;
           flex: 1;
@@ -36,12 +41,8 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;
+        .posts {
+          display: flex;
         }
       `}</style>
 
@@ -53,11 +54,23 @@ export default function Home() {
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
             Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
         }
-
         * {
           box-sizing: border-box;
         }
       `}</style>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetchEntries()
+  const posts = await res.map((p) => {
+    return p.fields
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
